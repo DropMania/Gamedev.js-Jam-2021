40ea343485956a8cdfd24fc7fa5a2950519e74c9
Robin <robinhallerbach13@googlemail.com>
2021-04-26 11:07:40 +0200

    Update sounds.js

--- public/src/sounds.js
+++ public/src/sounds.js
@@ -1,31 +1,43 @@
 let ost = new Audio('/src/assets/sounds/ost.mp3')
 ost.loop = true
 ost.volume = 0.4
+ost.crossOrigin = 'anonymous'
 
 let win = new Audio('/src/assets/sounds/win.mp3')
 win.volume = 1
+win.crossOrigin = 'anonymous'
 let fail = new Audio('/src/assets/sounds/fail.mp3')
 fail.volume = 1
+fail.crossOrigin = 'anonymous'
 let blick = new Audio('/src/assets/sounds/blick.mp3')
 blick.volume = 1
+blick.crossOrigin = 'anonymous'
 
 export let startOST = () => {
-    ost.play()
+    try {
+        ost.play()
+    } catch (e) {}
 }
 export let muteOST = () => {
     ost.muted = !ost.muted
 }
 export let playWin = () => {
-    win.play()
+    try {
+        win.play()
+    } catch (e) {}
 }
 export let playClick = () => {
-    blick.play()
-    setTimeout(() => {
-        blick.pause()
-        blick.currentTime = 0
-    }, 100)
+    try {
+        blick.play()
+        setTimeout(() => {
+            blick.pause()
+            blick.currentTime = 0
+        }, 100)
+    } catch (e) {}
 }
 export let playFail = () => {
-    fail.play()
+    try {
+        fail.play()
+    } catch (e) {}
 }
 export let ostIsMuted = ost.muted
