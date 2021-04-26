2b16b819834891658fe79cd275e547ac857dd688
Robin <robinhallerbach13@googlemail.com>
2021-04-26 10:33:49 +0200

    sound fix

--- public/src/sounds.js
+++ public/src/sounds.js
@@ -1,13 +1,17 @@
 let ost = new Audio('/src/assets/sounds/ost.mp3')
 ost.loop = true
 ost.volume = 0.4
+ost.crossOrigin = 'anonymous'
 
 let win = new Audio('/src/assets/sounds/win.mp3')
 win.volume = 1
+win.crossOrigin = 'anonymous'
 let fail = new Audio('/src/assets/sounds/fail.mp3')
 fail.volume = 1
+fail.crossOrigin = 'anonymous'
 let blick = new Audio('/src/assets/sounds/blick.mp3')
 blick.volume = 1
+blick.crossOrigin = 'anonymous'
 
 export let startOST = () => {
     ost.play()
